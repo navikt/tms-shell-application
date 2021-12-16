97e69ca2d0b9639cee6ec8c4083b069fcfc05507
Erlend <erlend.edvardsen.vige@nav.no>
2021-12-16 12:22:08 +0100

    Set correct namespace & team, remove dynamic import for now

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
 
-const MinSide = React.lazy(() =>
-  import("http://localhost:7100/dist/bundle.esm.js")
-);
-
 const renderMicrofrontend = (Microfrontend) => {
   return (
     <React.Suspense fallback="Loading...">
@@ -18,7 +14,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" render={() => renderMicrofrontend(MinSide)} />
+        <Route path="/" render={() => renderMicrofrontend(() => <p>Min Side</p>)} />
       </Switch>
     </Router>
   );
