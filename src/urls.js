b7502e0d987c91e623644df7aa8d434127eef012
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-01-12 14:42:30 +0100

    Bruker riktig url for å hente tms-min-side

--- src/urls.js
+++ src/urls.js
@@ -2,8 +2,8 @@ import { getEnvironment } from "./environment"
 
 const MIN_SIDE_URL = {
   local: "http://localhost:7100/tms-min-side/bundle.js",
-  development: "https://person.dev.nav.no/tms-min-side-topp/bundle.js",
-  production: "https://person.nav.no/tms-min-side-topp/bundle.js",
+  development: "https://person.dev.nav.no/tms-min-side/bundle.js",
+  production: "https://person.nav.no/tms-min-side/bundle.js",
 };
 
 export const minSideUrl = MIN_SIDE_URL[getEnvironment()];
