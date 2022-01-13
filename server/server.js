6c2de4ff9c8b9d13caed9565cf3f4bdaf215d35f
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-01-13 08:56:27 +0100

    Rydder bort debug logging

--- server/server.js
+++ server/server.js
@@ -1,24 +1,27 @@
 const express = require('express');
-const mustacheExpress = require("mustache-express");
-const path = require("path");
-const getDecorator = require("./dekorator");
-const basePath = "/min-side";
-const buildPath = path.resolve(__dirname, "../dist");
+const mustacheExpress = require('mustache-express');
+const path = require('path');
+const getDecorator = require('./dekorator');
+const basePath = '/min-side';
+const buildPath = path.resolve(__dirname, '../dist');
 const server = express();
 
-server.set("views", `${__dirname}/../dist`);
-server.set("view engine", "mustache");
-server.engine("html", mustacheExpress());
+server.set('views', `${__dirname}/../dist`);
+server.set('view engine', 'mustache');
+server.engine('html', mustacheExpress());
 
 server.use(express.json());
 server.use((req, res, next) => {
-    res.removeHeader("X-Powered-By");
+    res.removeHeader('X-Powered-By');
     next();
 });
 
-server.use(basePath, express.static(buildPath, {
-    index: false,
-}));
+server.use(
+    basePath,
+    express.static(buildPath, {
+        index: false,
+    })
+);
 
 server.get(`${basePath}/internal/isAlive`, async (req, res) => {
     res.sendStatus(200);
@@ -30,16 +33,14 @@ server.get(`${basePath}/internal/isReady`, async (req, res) => {
 
 // Match everything except internal og static
 server.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) =>
-  getDecorator()
-    .then((fragments) => {
-        console.log("sending index.html...");
-        res.render("index.html", fragments);
-    })
-    .catch((e) => {
-        const error = `Failed to get decorator: ${e}`;
-        console.log(e);
-        res.status(500).send(error);
-    })
+    getDecorator()
+        .then((fragments) => {
+            res.render('index.html', fragments);
+        })
+        .catch((e) => {
+            const error = `Failed to get decorator: ${e}`;
+            res.status(500).send(error);
+        })
 );
 
 const port = 7150;
